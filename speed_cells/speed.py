4d09477574edf6e0283867a3df95ffaf3dc6c7cb
Mikkel Elle Lepperød <lep.mik@gmail.com>
2019-03-29 14:33:51 +0100

    testing etc

--- speed_cells/speed.py
+++ speed_cells/speed.py
@@ -2,47 +2,85 @@ import numpy as np
 import quantities as pq
 import neo
 import elephant as ep
-import quantities as pq
+from scipy.interpolate import interp1d
 
 
-def speed_correlation(speed, t, sptr, stddev=250*pq.ms, min_speed=0.0*pq.m*pq.Hz,
-                      max_speed_percentile=100, return_data=False):
+def filter_speed_percentile(speed, times, min_speed=0.0, percentile=100):
+    """
+    Filters speed above percentile and belov min_speed.
+    Parameters:
+    -----------
+    speed : array
+        assumed m/s
+    times : array
+        assumed s
+    min_speed : float
+        lower cutoff
+    percentile : float
+        upper cutoff
+    Returns
+    -------
+    out : correlation, (pearson, inst_speed)
+    References
+    ----------
+    [1]:
+    """
+    min_speed = pq.Quantity(min_speed, 'm/s')
+    max_speed = np.percentile(speed, percentile)
+    mask = np.logical_and(speed < max_speed, speed > min_speed)
+    return speed[mask], times[:speed.size][mask]
+
+
+def speed_correlation(
+    speed, times, spike_times, stddev=0.25, filter_speed=True,
+    min_speed=0.0, percentile=100):
     """
     Correlates instantaneous spike rate and rat velocity, using a method
     described in [1]
     Parameters:
     -----------
-    speed, t :
-    sptr :
+    speed : array
+        assumed m/s
+    times : array
+        assumed s
+    spike_times : array or neo.SpikeTrain
+        assumed s
+    stddev : float
+        standard deviation of Gaussian kernel generating instantaneous spike rate
+    filter_speed : tuple
+        lower bound and upper percentile; see `filter_speed_percentile`
     Returns
     -------
     out : correlation, (pearson, inst_speed)
+    References
+    ----------
     [1]:
     """
+    times = pq.Quantity(times, 's')
+    speed = pq.Quantity(speed, 'm/s')
+    stddev = pq.Quantity(stddev, 's')
+    if filter_speed:
+        speed, times = filter_speed_percentile(
+            speed, times, min_speed, percentile)
+        spike_times = spike_times[spike_times < times[-1]]
 
-    from scipy.interpolate import interp1d
-    max_speed = np.percentile(speed, max_speed_percentile)
-    mask = np.logical_and(speed < max_speed, speed > min_speed)
-
-    t = t * pq.s
-
-    t_ = t[:speed.size][mask]
-    speed = speed[mask]
-
-    dt = np.average(np.diff(t_))
+    if not isinstance(spike_times, neo.SpikeTrain):
+        spike_times = neo.SpikeTrain(
+            spike_times, units='s', t_start=times[0], t_stop=times[-1])
 
     interp_speed = interp1d(
-        t_, speed,  bounds_error=False, fill_value=(speed[0], speed[-1]))
-    binsize = np.mean(np.diff(t)).rescale('s') * 2
+        times, speed,  bounds_error=False, fill_value=(speed[0], speed[-1]))
+    binsize = np.mean(np.diff(times)).rescale('s') * 2
 
     rate = ep.statistics.instantaneous_rate(
-        sptr, binsize, t_start = t[0], t_stop = t[-1],
+        spike_times, binsize,
         kernel=ep.kernels.GaussianKernel(sigma=stddev))
 
-    mask = np.logical_and(rate.times <= t[-1], rate.times >= t[0])
-    inst_speed = interp_speed(rate.times[mask].rescale('s'))
-    rate = rate.magnitude[mask,0]
+    mask = np.logical_and(rate.times <= times[-1], rate.times >= times[0])
+    times = rate.times[mask].rescale('s').magnitude
+    inst_speed = interp_speed(times)
+    rate = rate.magnitude[mask, 0]
     # rate = inst_rate.magnitude[:,0]
-    correlation = np.corrcoef(inst_speed, rate)[1,0]
+    correlation = np.corrcoef(inst_speed, rate)[1, 0]
 
-    return correlation, inst_speed, rate
+    return correlation, inst_speed, rate, times
--- speed_cells/tests/test_speed.py
+++ speed_cells/tests/test_speed.py
@@ -3,49 +3,91 @@ import pytest
 import quantities as pq
 import neo
 from tools import random_walk
-from spatial_maps.speed import speed_correlation
-# TODO need a better test, where we can set the corr beforehand
+from speed_cells.speed import speed_correlation
+
+
+def nonstationary_poisson(times, rate):
+    """
+    Non-stationary Poisson process
+    Implemented by Alexander Stasik (a.j.stasik@fys.uio.no)
+    Parameters
+    ----------
+    times : array
+        timepoints corresponding to rate.
+    rate : array
+        rates as function of time.
+    Returns
+    -------
+    events : array
+        time points from a poisson process which rate varies according to rate.
+    """
+    n_exp = rate.max() * (times.max()-times.min())
+    t_events = np.sort(
+        np.random.uniform(
+            times.min(), times.max(), np.random.poisson(n_exp)))
+    mask = np.digitize(t_events, times)
+    ratio = rate[mask] / rate.max()
+    mask = np.random.uniform(0., 1., len(ratio)) < ratio
+    return t_events[mask]
+
+
+def stationary_poisson(t_start, t_stop, rate):
+    """
+    Stationary Poisson process
+    Parameters
+    ----------
+    t_start : float
+        Start time of the process (lower bound).
+    t_stop : float
+        Stop time of the process (upper bound).
+    rate : float
+        rate of the Poisson process
+    Returns
+    -------
+    events : array
+        time points from a Poisson process with rate rate.
+    """
+    n_exp = rate * (t_stop - t_start)
+    return np.sort(
+        np.random.uniform(
+            t_start, t_stop, np.random.poisson(n_exp)))
+
 
 def test_speed_random():
     box_size = [1., 1.]
     rate = 5.
-    bin_size = [.01, .01]
     n_step=10**4
     step_size=.01
-
-    from elephant.spike_train_generation import homogeneous_poisson_process
-    from scipy.interpolate import interp1d
 
     t = np.linspace(0, n_step * step_size / 1.5, n_step)
     dt = t[1]
     trajectory = random_walk(box_size, step_size, n_step)
     x, y = trajectory.T
-    st = homogeneous_poisson_process(
-        rate=rate / pq.s, t_start=0 * pq.s, t_stop=t[-1] * pq.s)
+    st = stationary_poisson(
+        rate=rate, t_start=0, t_stop=t[-1])
     s = np.sqrt(x*x + y*y)
     speed = np.diff(s) / dt
-    corr, inst_speed, st_rate = speed_correlation(speed, t, st)
+    corr, inst_speed, inst_rate, times = speed_correlation(speed, t, st)
     assert  np.abs(corr) < 0.05
-    assert st_rate.mean().round() == rate
+    assert inst_rate.mean().round() == rate
+
+
+def test_speed_nonlinear():
+    sampling_rate = 50 #hz
+    f, a = .1, 1
+    t = np.arange(0, 100, 1 / sampling_rate)
+    speed = a * (np.sin(2*np.pi*f*t) + 1)
+    spikes = nonstationary_poisson(t, speed)
+    corr, inst_speed, inst_rate, times = speed_correlation(
+        speed, t, spikes, stddev=.4, filter_speed=True, percentile=95)
+    assert  round(corr, 2) > 0.5
 
 
 def test_speed_linear():
-    box_size = [1., 1.]
-    n_step=10**3
-
-    from scipy.interpolate import interp1d
-
-    t = np.linspace(0, n_step * .1, n_step)
-    speed = np.linspace(0, 5, n_step)
-    dt = 1
-    s = 0
-    spikes = []
-    while dt > .001:
-        dt /= 1.01
-        next_s = s + dt
-        spikes.append(next_s)
-        s = next_s
-    spikes = neo.SpikeTrain(spikes, t_stop=round(spikes[-1]), units='s')
-
-    corr, inst_speed, st_rate = speed_correlation(speed, t, spikes)
-    assert  round(corr, 2) == 0.36
+    sampling_rate = 50 #hz
+    t = np.arange(0, 100, 1 / sampling_rate)
+    speed = np.linspace(0, 5, len(t))
+    spikes = nonstationary_poisson(t, speed)
+    corr, inst_speed, inst_rate, times = speed_correlation(
+        speed, t, spikes, stddev=.4, filter_speed=True, percentile=95)
+    assert  round(corr, 2) > 0.5
